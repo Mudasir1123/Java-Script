dd718e02b9c98f43c0d38765413ffe6d770fa080
yasir <yasirmalik1127@gmail.com>
2023-10-26 21:19:36 +0500

    submit

---
+++ app.js
@@ -0,0 +1,167 @@
+alert("Error! Please enter a valid passwoard.");
+
+alert("Welcome to JS land... \n Happy coding");
+// // window.alert("Welcome to JS land...  Happy coding")
+
+
+alert("Welcome to JS land...");
+
+var first = ("Hello...")
+var last = ("I can run JS throgh my web browser's console")
+var full = first + "" + last 
+alert(full)
+
+alert('Jhone doe');
+
+alert("15 years old");
+alert("Certified Mobile Application Development");
+alert(" Pizza \n Pizz \n Piz \n Pi \n Pi \n P");
+alert("My Email Address is Example@example.com ");
+alert("I am trying to learn from the book A smarter \n way to learn to java script ");
+alert("You have visited this site 14 times");
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+var num = 3+5
+console.log(num)
+
+var num = 3*5
+console.log(num)
+
+var num = 25-10
+console.log(num)
+
+var num = 25/2
+console.log(num)
+
+var num = 600*5
+console.log(num)
+
+var num = 5
+num = num + 1
+num = num + 7
+num = num - 1
+num = num /4
+num = num -3
+console.log(num)
+
+
+var a = 1*4
+console.log(a)
+var a = 2*4
+console.log(a)
+var a = 3*4
+console.log(a)
+var a = 4*4
+console.log(a)
+var a = 5*4
+console.log(a)
+var a = 6*4
+console.log(a)
+var a = 7*4
+console.log(a)
+var a = 8*4
+console.log(a)
+var a = 9*4
+console.log(a)
+var a = 10*4
+console.log(a)
+
+var c = (77-32)*5/9
+console.log (c)
+var c = (25*9/5)+32
+console.log (c)
+var item1 = 650*3
+console.log(item1)
+var item2 = 100*7
+console.log(item2)
+var Shippingcharges = 100
+console.log(item1 + item2 + Shippingcharges)
+
+
+var presantage = (804/980)*100
+console.log(presantage) 
+
+var Dollar = 10*104.80
+var SaudiRiyal = 25*28
+console.log(Dollar + SaudiRiyal) 
+
+var num = (5*10)/2
+console.log(num)
+
+var age = 2016-1992
+console.log(age)
+
+var num = ("apple")
+var age = 15
+var max = 65
+var num1 = 3
+console.log(num+age+max-num1)
+
+
+var num = 10
+console.log(num++)
+console.log(num)
+console.log(num++)
+console.log(num)
+console.log(num--)
+console.log(num)
+console.log(num--)
+console.log(num)
+
+var r = 2*3.142*20
+console.log(r)
+
+var r = 3.142*20*20
+console.log(r)
+
+var num = 2
+console.log(--num)
+
+
+var str="Assalam o Alaikum"
+var userName = prompt("Enter your name")
+console.log(str + " " + userName)
+
+
+
+ var gen = prompt("Gender")    
+ if (gen == "male"){
+     console.log("Good morning Sir")
+} 
+else if (gen =="female"){
+  console.log("Good morning maam")
+}
+  else{
+     console.log("")
+  }
+    
+
+var color = "red"
+
+var color = prompt ("Enter your color")
+
+if(color=="red"){
+    alert("must stop")
+}
+
+else if(color == "yellow") {
+    alert("ready to move")
+}
+else if (color== "green"){
+    alert("Go")
+}
+else{
+    alert("Ruk ja koi color nahi h")
+}
+
+var roll = prompt("city name")
+if (roll == "karachi"){
+console.log("Welcome to city of light")
+}else {
+ console.log("none")
+}
+
+
+var presantage = (156/300)*100
+console.log("presantage")
